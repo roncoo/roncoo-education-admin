75f6a2f301ea915cd2109956267918534aba7d8b
fengyw <fengyw@roncoo.com>
2024-02-25 21:42:11 +0800

    docs: 升级优化，全面使用vue3语法

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -5,18 +5,12 @@
         <el-form :model="query" inline label-width="80px">
           <el-form-item>
             <el-button @click="resetQuery()">刷新</el-button>
-            <el-button plain type="success" @click="openFormModal(null, '0')">添加</el-button>
+            <el-button v-permission="'sys:menu:save'" plain type="success" @click="openFormModal(null, '0')">添加</el-button>
           </el-form-item>
         </el-form>
       </div>
     </div>
-    <el-table
-      :data="page.list"
-      v-loading="page.loading"
-      :tree-props="{ children: 'childrenList', hasChildren: 'hasChildren' }"
-      border
-      row-key="id"
-    >
+    <el-table :data="page.list" v-loading="page.loading" :tree-props="{ children: 'childrenList', hasChildren: 'hasChildren' }" border row-key="id">
       <el-table-column label="名称" min-width="60" prop="menuName">
         <template #default="scope"> &nbsp;{{ scope.row.menuName }} </template>
       </el-table-column>
@@ -43,21 +37,13 @@
       <el-table-column label="排序" min-width="50" prop="sort" />
       <el-table-column label="状态" min-width="50">
         <template #default="scope">
-          <span :class="{ red: scope.row.statusId === 0 }">{{
-            getEnumObj('StatusIdEnum')[scope.row.statusId]
-          }}</span>
+          <span :class="{ red: scope.row.statusId === 0 }">{{ getEnumObj('StatusIdEnum')[scope.row.statusId] }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" width="280">
         <template #default="scope">
-          <el-button
-            v-if="scope.row.menuType !== 3"
-            plain
-            type="success"
-            @click="openFormModal(null, scope.row.id)"
-            >新增</el-button
-          >
-          <el-button plain type="primary" @click="openFormModal(scope.row)">编辑</el-button>
+          <el-button v-permission="'sys:menu:save'" v-if="scope.row.menuType !== 3" plain type="success" @click="openFormModal(null, scope.row.id)">新增</el-button>
+          <el-button v-permission="'sys:menu:edit'" plain type="primary" @click="openFormModal(scope.row)">编辑</el-button>
           <el-dropdown>
             <el-button>
               更多操作
@@ -67,23 +53,11 @@
             </el-button>
             <template #dropdown>
               <el-dropdown-menu>
-                <el-dropdown-item>
-                  <el-button
-                    v-if="scope.row.statusId === 1"
-                    plain
-                    type="danger"
-                    @click="handleStatus(scope.row)"
-                    >禁用</el-button
-                  >
-                  <el-button
-                    v-if="scope.row.statusId === 0"
-                    plain
-                    type="success"
-                    @click="handleStatus(scope.row)"
-                    >启用</el-button
-                  >
+                <el-dropdown-item v-permission="'sys:menu:edit'">
+                  <el-button v-if="scope.row.statusId === 1" plain type="danger" @click="handleStatus(scope.row)">禁用</el-button>
+                  <el-button v-if="scope.row.statusId === 0" plain type="success" @click="handleStatus(scope.row)">启用</el-button>
                 </el-dropdown-item>
-                <el-dropdown-item>
+                <el-dropdown-item v-permission="'sys:menu:delete'">
                   <el-button plain type="danger" @click="handleDelete(scope.row)">删除</el-button>
                 </el-dropdown-item>
               </el-dropdown-menu>
@@ -97,24 +71,24 @@
 </template>
 
 <script setup>
-import useTable from '@/utils/table'
-import { reactive, ref } from 'vue'
-import FormModel from './FormModel.vue'
-import { systemApi } from '@/api/system'
-import { getEnumObj } from '@/utils/base'
+  import useTable from '@/utils/table'
+  import { reactive, ref } from 'vue'
+  import FormModel from './FormModel.vue'
+  import { systemApi } from '@/api/system'
+  import { getEnumObj } from '@/utils/base'
 
-// 添加/修改
-const formRef = ref()
-const openFormModal = (item, parentId) => {
-  formRef.value.onOpen(item, parentId)
-}
+  // 添加/修改
+  const formRef = ref()
+  const openFormModal = (item, parentId) => {
+    formRef.value.onOpen(item, parentId)
+  }
 
-// 基础功能
-const { page, handlePage, query, resetQuery, handleDelete, handleStatus } = reactive({
-  ...useTable({
-    page: systemApi.sysMenuList,
-    delete: systemApi.sysMenuDelete,
-    status: systemApi.sysMenuEdit
+  // 基础功能
+  const { page, handlePage, query, resetQuery, handleDelete, handleStatus } = reactive({
+    ...useTable({
+      page: systemApi.sysMenuList,
+      delete: systemApi.sysMenuDelete,
+      status: systemApi.sysMenuEdit
+    })
   })
-})
 </script>
