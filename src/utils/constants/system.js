63e921651e71db7d424851a94f33338b75fc6548
fengyw <fengyw@roncoo.com>
2024-02-27 11:36:27 +0800

    docs: 升级优化，全面使用vue3语法

--- src/utils/constants/system.js
+++ src/utils/constants/system.js
@@ -1,17 +1,11 @@
 /*
  * 通用常量
- *
- * @Author:    1024创新实验室-主任：卓大
- * @Date:      2022-09-06 19:57:29
- * @Wechat:    zhuda1024
- * @Email:     lab1024@163.com
- * @Copyright  1024创新实验室 （ https://1024lab.net ），Since 2012
  */
 export const PATH = {
-    URL_LOGIN: '/login',
-    URL_404: '/404',
-    URL_403: '/403',
-    URL_GATEWAY: '/gateway',
-    URL_DASHBOARD: '/dashboard'
+  URL_LOGIN: '/login',
+  URL_404: '/404',
+  URL_403: '/403',
+  URL_GATEWAY: '/gateway',
+  URL_DASHBOARD: '/dashboard'
 }
 export const TOKEN_KEY = 'EDU_OS_TOKEN'
