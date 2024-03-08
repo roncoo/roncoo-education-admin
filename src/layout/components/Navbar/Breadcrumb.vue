204c84071a462981e314d8db98beeb7f768b307c
fengyw <fengyw@roncoo.com>
2024-03-08 08:42:53 +0800

    docs: 升级优化，全面使用vue3语法

--- src/layout/components/Navbar/Breadcrumb.vue
+++ src/layout/components/Navbar/Breadcrumb.vue
@@ -1,34 +1,32 @@
 <template>
   <el-breadcrumb :separator-icon="ArrowRight">
-    <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index">{{
-      item.title
-    }}</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index">{{ item.title }}</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup>
-import { ArrowRight } from '@element-plus/icons-vue'
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
-import { useUserStore } from '@/store/modules/user'
+  import { ArrowRight } from '@element-plus/icons-vue'
+  import { computed } from 'vue'
+  import { useRoute } from 'vue-router'
+  import { useUserStore } from '@/store/modules/user'
 
-let route = useRoute()
+  const route = useRoute()
 
-const breadcrumbList = computed(() => {
-  let menuId = route.name
-  if (!menuId) {
-    return []
-  }
-  return useUserStore().getBreadcrumbMap.get(menuId)
-})
+  const breadcrumbList = computed(() => {
+    let menuId = route.name
+    if (!menuId) {
+      return []
+    }
+    return useUserStore().getBreadcrumbMap.get(menuId)
+  })
 </script>
 
 <style lang="scss" scoped>
-.el-breadcrumb {
-  display: inline-block;
-  vertical-align: top;
-  margin-left: 20px;
-  line-height: 50px;
-  font-size: 14px;
-}
+  .el-breadcrumb {
+    display: inline-block;
+    vertical-align: top;
+    margin-left: 20px;
+    line-height: 50px;
+    font-size: 14px;
+  }
 </style>
