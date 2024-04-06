a32e9e6165ff46c40ea4d3a2991ecf74e8305014
fengyw <fengyw@roncoo.com>
2024-04-06 21:36:47 +0800

    docs: 升级优化，全面使用vue3语法

---
+++ src/views/dashboard/StatData.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-card class="info">
+    <template #header>数据统计</template>
+    <el-row>
+      <el-col :span="6">
+        <router-link to="/common/order">
+          <el-statistic title="今天订单数" :value="statData.todayOrder" />
+        </router-link>
+      </el-col>
+      <el-col :span="6">
+        <router-link to="/common/order">
+          <el-statistic title="今天收入" :precision="2" :value="statData.todayMoney" :prefix="'￥'" />
+        </router-link>
+      </el-col>
+      <el-col :span="6">
+        <router-link to="/common/order">
+          <el-statistic title="昨天订单数" :value="statData.yesterdayOrder" />
+        </router-link>
+      </el-col>
+      <el-col :span="6">
+        <router-link to="/common/order">
+          <el-statistic title="昨天收入" :precision="2" :value="statData.yesterdayMoney" :prefix="'￥'" />
+        </router-link>
+      </el-col>
+    </el-row>
+    <br />
+    <el-row>
+      <el-col :span="6">
+        <router-link to="/users/list">
+          <el-statistic title="总用户数" :value="statData.userSum" />
+        </router-link>
+      </el-col>
+      <el-col :span="6">
+        <router-link to="/course/list">
+          <el-statistic title="总课程数" :value="statData.courseSum" />
+        </router-link>
+      </el-col>
+      <el-col :span="6">
+        <router-link to="/common/order">
+          <el-statistic title="总订单数" :value="statData.orderSum" />
+        </router-link>
+      </el-col>
+      <el-col :span="6">
+        <router-link to="/common/order">
+          <el-statistic title="总收入" :precision="2" :value="statData.orderMoney" :prefix="'￥'" />
+        </router-link>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script setup>
+  import { onMounted, ref } from 'vue'
+  import { statApi } from '@/api/dashboard.js'
+
+  const statData = ref({})
+  onMounted(() => {
+    statApi.data().then((res) => {
+      statData.value = res
+    })
+  })
+</script>
+<style lang="scss" scoped>
+  .info {
+    padding: 15px;
+    border-radius: 5px;
+    font-size: 16px;
+  }
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,55 +1,8 @@
 <template>
   <div class="dashboard-container">
     <el-row :gutter="20">
-      <el-col :span="16">
-        <el-card v-permission="'stat:data'" class="info">
-          <template #header>数据统计</template>
-          <el-row>
-            <el-col :span="6">
-              <router-link to="/common/order">
-                <el-statistic title="今天订单数" :value="statData.todayOrder" />
-              </router-link>
-            </el-col>
-            <el-col :span="6">
-              <router-link to="/common/order">
-                <el-statistic title="今天收入" :precision="2" :value="statData.todayMoney" :prefix="'￥'" />
-              </router-link>
-            </el-col>
-            <el-col :span="6">
-              <router-link to="/common/order">
-                <el-statistic title="昨天订单数" :value="statData.yesterdayOrder" />
-              </router-link>
-            </el-col>
-            <el-col :span="6">
-              <router-link to="/common/order">
-                <el-statistic title="昨天收入" :precision="2" :value="statData.yesterdayMoney" :prefix="'￥'" />
-              </router-link>
-            </el-col>
-          </el-row>
-          <br />
-          <el-row>
-            <el-col :span="6">
-              <router-link to="/users/list">
-                <el-statistic title="总用户数" :value="statData.userSum" />
-              </router-link>
-            </el-col>
-            <el-col :span="6">
-              <router-link to="/course/list">
-                <el-statistic title="总课程数" :value="statData.courseSum" />
-              </router-link>
-            </el-col>
-            <el-col :span="6">
-              <router-link to="/common/order">
-                <el-statistic title="总订单数" :value="statData.orderSum" />
-              </router-link>
-            </el-col>
-            <el-col :span="6">
-              <router-link to="/common/order">
-                <el-statistic title="总收入" :precision="2" :value="statData.orderMoney" :prefix="'￥'" />
-              </router-link>
-            </el-col>
-          </el-row>
-        </el-card>
+      <el-col v-permission="'stat:data'" :span="16">
+        <stat-data />
       </el-col>
       <el-col :span="8">
         <el-card class="info">
@@ -72,16 +25,7 @@
 <script setup>
   import StatVod from './StatVod.vue'
   import StatLogin from './StatLogin.vue'
-  import { onMounted, ref } from 'vue'
-  import { statApi } from '@/api/dashboard.js'
-
-  const statData = ref({})
-
-  onMounted(() => {
-    statApi.data().then((res) => {
-      statData.value = res
-    })
-  })
+  import StatData from './StatData.vue'
 </script>
 <style lang="scss" scoped>
   .info {
