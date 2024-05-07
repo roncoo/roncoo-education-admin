d8b4660cadbddcb8349286d6dc3753c76e097f54
fengyw <fengyw@roncoo.com>
2024-05-07 20:25:23 +0800

    docs: 升级优化，全面使用vue3语法

--- src/components/Upload/File/index.vue
+++ src/components/Upload/File/index.vue
@@ -73,7 +73,8 @@
   /* 类型设置 */
   const vodType = 'video/mp4,video/avi'
   const audioType = 'audio/mpeg,audio/mp3,audio/mp4'
-  const docType = 'application/pdf,application/msword,application/vnd,application/x-xls,text/plain'
+  const docType =
+    'application/pdf,application/msword,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/x-xls,text/plain'
   const imageType = 'image/jpeg,image/png,image/gif,image/x-icon'
   const zipType = 'application/zip,application/gzip,application/x-zip-compressed'
 
