25b15631561e22cb753002d5ad83259cb5106371
fengyw <fengyw@roncoo.com>
2024-05-16 21:55:20 +0800

    feat: 使用nuxt3进行项目重构

--- src/views/course/list/index.vue
+++ src/views/course/list/index.vue
@@ -21,7 +21,7 @@
       <el-table-column :min-width="180" label="课程信息">
         <template #default="scope">
           <img :alt="scope.row.courseName" :src="scope.row.courseLogo" style="float: left; height: 50px; width: auto; vertical-align: middle; border-radius: 5px" />
-          <div style="float: left; margin-left: 10px">
+          <div style="float: left; margin-left: 10px; max-width: 70%">
             <el-link :href="'/course/detail?id=' + scope.row.id" target="_blank">{{ scope.row.courseName }}</el-link>
             <br />
             <span v-if="scope.row.isFree == 1">免费</span>
