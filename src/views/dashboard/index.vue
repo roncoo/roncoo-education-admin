f005a9524e438fb3fb9559e3dd4b1daaeb6db4a5
fengyw <fengyw@roncoo.com>
2024-06-01 08:42:23 +0800

    feat: 使用nuxt3进行项目重构

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="dashboard-container">
     <el-row :gutter="20">
-      <el-col v-if="hasPermission('stat:data')" :span="16">
+      <el-col v-if="hasPermission('stat:data')" :span="14">
         <stat-data />
       </el-col>
-      <el-col :span="8">
+      <el-col :span="10">
         <el-card class="info">
           <template #header>特别说明</template>
-          <p>1. 演示环境仅提供查看功能，更多功能需本地部署</p>
-          <p>2. 系统如何搭建部署？视频如何实现本地私有化？</p>
-          <p>3. 作者提供有偿指导服务：18302045627(微信同号)</p>
+          <div class="info-body">
+            <div class="info-content">
+              <div>1. 演示环境仅有查看功能，更多体验需本地部署</div>
+              <div>2. 系统如何搭建部署？视频如何实现本地私有化？</div>
+              <div>3. 作者提供有偿服务：18302045627(微信同号)</div>
+              <div>4. 关注公众号，获取更多资料：SQL脚本、教程</div>
+            </div>
+            <img class="info-image" src="@/assets/images/weixin.jpg" alt="作者微信公众号" style="width: auto; height: 120px" />
+          </div>
         </el-card>
       </el-col>
     </el-row>
@@ -33,5 +39,15 @@
     padding: 15px;
     border-radius: 5px;
     font-size: 16px;
+    .info-body {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .info-content {
+      div {
+        line-height: 32px;
+      }
+    }
   }
 </style>
